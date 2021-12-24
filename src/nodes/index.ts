53944d879aa49b6a419fc4eb0e77ddeb86e8567d
KJ Shanks <kyle.j.shanks@gmail.com>
2021-12-24 04:03:03 -0500

    Add index file for nodes folder to export nodes

---
+++ src/nodes/index.ts
@@ -0,0 +1,23 @@
+// Core Nodes
+import { ChannelMerger } from './core/ChannelMerger'
+import { ChannelSplitter } from './core/ChannelSplitter'
+import { Compressor } from './core/Compressor'
+import { Convolver } from './core/Convolver'
+import { Delay } from './core/Delay'
+import { Filter } from './core/Filter'
+import { Gain } from './core/Gain'
+import { StereoPanner } from './core/StereoPanner'
+import { WaveShaper } from './core/WaveShaper'
+
+export {
+  // Core Nodes
+  ChannelMerger,
+  ChannelSplitter,
+  Compressor,
+  Convolver,
+  Delay,
+  Filter,
+  Gain,
+  StereoPanner,
+  WaveShaper,
+}
